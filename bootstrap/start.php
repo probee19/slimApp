dee955f4765e47c0cb79afe1a89fd0583985ad2c
babacar <babacar@3w.lagence.de.com>
2018-03-13 11:41:24 +0000

    autoload call on start

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -6,10 +6,15 @@ use Illuminate\Database\Capsule\Manager;
 use Slim\App;
 
 
+session_start();
+// RDS CONST
 define('RDS_HOSTNAME', $_SERVER['RDS_HOSTNAME']);
 define('RDS_USERNAME', $_SERVER['RDS_USERNAME']);
 define('RDS_PASSWORD', $_SERVER['RDS_PASSWORD']);
-define('RDS_DB_NAME', $_SERVER['RDS_DB_NAME']);
+define('RDS_DB_NAME',  $_SERVER['RDS_DB_NAME']);
+
+require __DIR__ . '/../vendor/autoload.php';
+
 $app = new App([
     'settings' => [
         'displayErrorDetails'   =>  true,
