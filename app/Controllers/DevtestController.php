53f7bb49971e68b6ee3c33c6f663fe9093456dd3
babacar <babacar@3w.lagence.de.com>
2018-03-29 17:59:42 +0000

    upload to s3 tests

--- app/Controllers/DevtestController.php
+++ app/Controllers/DevtestController.php
@@ -62,8 +62,6 @@ class DevtestController extends Controller
                     'Key' =>  $keyName,
                     'SourceFile' => $tempFilePath,
                     'StorageClass' => 'REDUCED_REDUNDANCY',
-                    'ACL'    => 'public-read',
-
                 )
             );
             var_dump($res);
