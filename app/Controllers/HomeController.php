98ef3682d0883a3a7e60ddbafa6a3ea47d0519a7
babacar <babacar@3w.lagence.de.com>
2018-05-29 16:16:48 +0000

    changing base_url

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -129,7 +129,7 @@ class HomeController extends Controller
       //$this->helper->debug($all_lang);
       if($_GET['debug'] && $_GET['debug'] == true)
         $this->helper->debug($tests);
-      return $this->view->render($response, 'home.twig', compact('tests_on_top', 'lang', 'tests', 'pagecount', 'pageid', 'mode', 'interface_ui', 'all_lang'));
+      return $this->view->render($response, 'home.twig', compact('tests_on_top', 'lang', 'tests', 'pagecount', 'pageid', 'interface_ui', 'all_lang'));
 
     }
 
