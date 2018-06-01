bc2bff4799f53b572e5bc43794bf75a6484c0b7d
Pedre Dieye <pedre@3w.lagence.de.com>
2018-06-01 12:39:19 +0000

    Upgrading file in ./creation-test

--- creation-test/app/Helpers/Helper.php
+++ creation-test/app/Helpers/Helper.php
@@ -279,6 +279,109 @@ class Helper
       return $texte;
     }
 
+    // Crée le fichier php à exécuter pour capturer l'image d'une citation
+    public static function getUrlCitationFile($lang, $citation_id, $path, $php, $css, $js, $head, $bottom)
+    {
+      // Create a temporary file for execute php code
+      $url_temp_file_php = $lang.'_file_citation_'.$citation_id;
+      $temp_file_php = fopen($path . $url_temp_file_php.".php", "w+");
+      if($temp_file_php==false)
+      die("La création du fichier a échoué");
+
+      //
+      $php = str_replace('{%additionnal_input_text%}', '$_GET[\'additionnal_input_text\']', $php );
+      //
+      // Traduction dans la langue $lang
+      //if($lang != 'fr')
+        $php = self::translateWithTags($lang, $php);
+
+      // changement de tags pour le code php
+      $code_php = $php;
+      $code_php = str_replace('{%', '$_GET[\'', $code_php );
+      $code_php = str_replace('%}', '\']', $code_php );
+
+      // changement de tags pour les variables
+      $code_php = str_replace('{?', '\'.$_GET[\'', $code_php );
+      $code_php = str_replace('?}', '\'].\'', $code_php );
+
+      // changement de tags pour le code html
+      $code_php = str_replace('{{', '<?php echo $_GET[\'', $code_php );
+      $code_php = str_replace('}}', '\']; ?>', $code_php );
+      $begin_gabarit = "
+          <!DOCTYPE html>
+          <html lang='en'>
+          <head>
+              <meta charset='UTF-8'>
+              <meta name='viewport' content='width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0'>
+              <meta http-equiv='X-UA-Compatible' content='ie=edge'>
+              ".$head."
+              <title>Theme 4</title>
+              <style>
+                  body{font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif;}
+                  .main{
+                      padding:0;
+                      margin:0;
+                      width: 800px;
+                      height:420px;
+                      position: relative;
+                      overflow: hidden;
+                  }
+              .texte2{
+                width: 200px;
+              }
+              ".$css."
+              </style>
+              <script src='../../../src/js/jquery.js'></script>
+              <script>
+                  $(document).ready(function(){
+                var autoSizeText;
+                autoSizeText = function() {
+                  var el, elements, _i, _len, _results;
+                  elements = $('.texte2');
+                  console.log(elements);
+                  if (elements.length < 0) {
+                    return;
+                  }
+                  _results = [];
+                  for (_i = 0, _len = elements.length; _i < _len; _i++) {
+                    el = elements[_i];
+                    _results.push((function(el) {
+                      var resizeText, _results1;
+                      resizeText = function() {
+                        var elNewFontSize;
+                        elNewFontSize = (parseInt($(el).css('font-size').slice(0, -2)) - 1) + 'px';
+                        return $(el).css('font-size', elNewFontSize);
+                      };
+                      _results1 = [];
+                      while (el.scrollHeight > el.offsetHeight) {
+                        _results1.push(resizeText());
+                      }
+                      return _results1;
+                    })(el));
+                  }
+                  return _results;
+                };
+                autoSizeText();
+                  ".$js."
+
+                  });
+              </script>
+          </head>
+          <body style='width: 800px; height:420px; margin:0; padding:0; overflow: hidden;'>
+          <div class='main'>
+      ";
+
+      $end_gabarit = "
+        </div>
+        ".$bottom."
+        </body>
+        </html>
+      ";
+
+      fputs($temp_file_php, $begin_gabarit. "\n" . $code_php . "\n" . $end_gabarit);
+      //fputs($temp_file_php, $begin_gabarit. "<style>".$css."</style> \n" . "<script>".$js."</script> \n" . $code_php . $end_gabarit);
+      return $path . $url_temp_file_php . '.php';
+    }
     // Crée le fichier du test dans la langue $lang pour le test $test_id
     public static function getUrlTempFile($lang, $test_id, $path, $php, $css, $js, $head, $bottom)
     {
