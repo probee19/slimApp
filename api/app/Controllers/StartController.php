28b53a3279068cdf28d36cac60187c0d048a5554
Pedre Dieye <pedre@3w.lagence.de.com>
2018-06-07 16:58:28 +0000

    Including AWS S3

--- api/app/Controllers/StartController.php
+++ api/app/Controllers/StartController.php
@@ -166,8 +166,8 @@ class StartController extends Controller
           $generated = $this->grabzit->URLToImage($url, $options);
           $save = $this->grabzit->SaveTo("../".$filepath);
           if($save){
-              $filepath = "https://".$this->base_domain . "uploads/game_". $_POST['game'] . '.jpg';
-              $resultUrl = $this->helper->uploadToS3($filepath, 'api/');
+              $filepath_ = "https://".$this->base_domain . $filepath;
+              $resultUrl = $this->helper->uploadToS3($filepath_, 'api/');
           }
           $data = [
               'messenger_user_id'     => $user_id,
