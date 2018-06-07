7cacaf2b689908b9d9ad1835731eaf2ee1894e64
Pedre Dieye <pedre@3w.lagence.de.com>
2018-06-07 12:11:50 +0000

    Testing app

--- api/app/routes.php
+++ api/app/routes.php
@@ -4,3 +4,4 @@
 $app->get('/', 'ClickController:index');
 $app->get('/chunk', 'HomeController:index');
 $app->post('/start/{ref}', 'StartController:index');
+$app->post('/match', 'HomeController:createPicMatch');
