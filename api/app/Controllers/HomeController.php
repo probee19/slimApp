e4b5cf785570bc7408ae1b29847a37137e790898
Pedre <pedre@3w.lagence.de.com>
2018-06-08 11:46:51 +0000

    Debugging

--- api/app/Controllers/HomeController.php
+++ api/app/Controllers/HomeController.php
@@ -151,9 +151,7 @@ class HomeController extends Controller
       $ch = curl_init();
       curl_setopt($ch, CURLOPT_URL, $url);
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
       curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
-      curl_setopt($ch, CURLOPT_HTTPHEADER, array('X-HTTP-Method-Override: GET'));
       curl_setopt($ch, CURLOPT_POST, 1);
 
 
