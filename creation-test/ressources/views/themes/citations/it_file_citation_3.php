d5bc0a9dc020a6ccfd1097d1d5706d00d16c4c41
Pedre <pedre@3w.lagence.de.com>
2018-06-10 21:54:34 +0000

    Adding Citation Module

---
+++ creation-test/ressources/views/themes/citations/it_file_citation_3.php
@@ -0,0 +1,87 @@
+
+          <!DOCTYPE html>
+          <html lang='en'>
+          <head>
+              <meta charset='UTF-8'>
+              <meta name='viewport' content='width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0'>
+              <meta http-equiv='X-UA-Compatible' content='ie=edge'>
+              <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+              <title>Theme 4</title>
+              <style>
+                  body{font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif;}
+                  .main{
+                      padding:0;
+                      margin:0;
+                      width: 800px;
+                      height:420px;
+                      position: relative;
+                      overflow: hidden;
+                  }
+              .texte2{
+                width: 200px;
+              }
+              .main{ padding:0;margin:0;width: 800px;height:420px;position: relative;overflow: hidden; background: #FFF;}
+.main img{ position:absolute; max-height:420px; max-width:800px; }
+
+#back{position:absolute; z-index:1; left:0; top:0;  width:800px;  }         
+.overlay{position:absolute; z-index:1; left:0; top:0;  width:800px; height:420px; background:#000; opacity:0.6;}
+
+#logo_funizi{position:absolute; z-index:1; left:342px; top:10px;  max-width:800px; max-height:420px; }
+#citation{z-index:1; font-family: 'Roboto Condensed', sans-serif; position: absolute; left: 50px; bottom: 50px; color:#FFF; font-size:45px; line-height:55px; width:700px ; height:300px ;display:flex; align-items:center; justify-content:center; text-align:center; }
+       
+              </style>
+              <script src='../../../src/js/jquery.js'></script>
+              <script>
+                  $(document).ready(function(){
+                var autoSizeText;
+                autoSizeText = function() {
+                  var el, elements, _i, _len, _results;
+                  elements = $('.texte2');
+                  console.log(elements);
+                  if (elements.length < 0) {
+                    return;
+                  }
+                  _results = [];
+                  for (_i = 0, _len = elements.length; _i < _len; _i++) {
+                    el = elements[_i];
+                    _results.push((function(el) {
+                      var resizeText, _results1;
+                      resizeText = function() {
+                        var elNewFontSize;
+                        elNewFontSize = (parseInt($(el).css('font-size').slice(0, -2)) - 1) + 'px';
+                        return $(el).css('font-size', elNewFontSize);
+                      };
+                      _results1 = [];
+                      while (el.scrollHeight > el.offsetHeight) {
+                        _results1.push(resizeText());
+                      }
+                      return _results1;
+                    })(el));
+                  }
+                  return _results;
+                };
+                autoSizeText();
+                  //JavaScript
+
+                  });
+              </script>
+          </head>
+          <body style='width: 800px; height:420px; margin:0; padding:0; overflow: hidden;'>
+          <div class='main'>
+      
+<!DOCTYPE HTML>
+
+<img src="https://creation.funizi.com/images-theme-perso/1527609534.jpg" id="back"> 
+<div class="overlay"></div>
+
+<img src="https://creation.funizi.com/images-theme-perso/1527246526.png" id="logo_funizi"> 
+ 
+<div class="texte" id="citation">La vita metterà pietre sulla tua strada. <br> Decidi di costruire muri o ponti.</div>
+
+
+
+        </div>
+        
+        </body>
+        </html>
+      
