a76901b075f208a44fd00cad3d6e4fae6920e9cb
Pedre <pedre@3w.lagence.de.com>
2018-06-10 03:49:29 +0000

    Setting sharing page for FootBot

--- api/app/Controllers/FootBotController.php
+++ api/app/Controllers/FootBotController.php
@@ -17,7 +17,8 @@ class FootBotController extends Controller
           'btn_title'   =>  'FootBot',
           'user'        =>  $_GET['from'],
           'img_url'     =>  $_GET['img_url'],
-          'imgtest'     =>  urlencode('https://s3.us-east-2.amazonaws.com/funiziuploads/api/pronostics/pronostic_kO1S4NZlxFIPIp9.jpg')
+          'imgtest'     =>  urlencode('https://s3.us-east-2.amazonaws.com/funiziuploads/api/pronostics/pronostic_kO1S4NZlxFIPIp9.jpg'),
+          'url'         =>  urlencode('https://funizi.com/api/footbot?from=Pedre&img_url=https%3A%2F%2Fs3.us-east-2.amazonaws.com%2Ffuniziuploads%2Fapi%2Fpronostics%2Fpronostic_kO1S4NZlxFIPIp9.jpg')
           ];
         Helper::debug($data);
         return $this->view->render($response, 'footBot.twig', compact('data'));
