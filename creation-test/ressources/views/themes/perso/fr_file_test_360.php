7ff3ba4ff37151c81e09c82ec4b0f1416fa608d0
Pedre <pedre@3w.lagence.de.com>
2018-06-11 13:06:27 +0000

    Adding test files

--- creation-test/ressources/views/themes/perso/fr_file_test_360.php
+++ creation-test/ressources/views/themes/perso/fr_file_test_360.php
@@ -5,7 +5,7 @@
               <meta charset='UTF-8'>
               <meta name='viewport' content='width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0'>
               <meta http-equiv='X-UA-Compatible' content='ie=edge'>
-              <link href="https://fonts.googleapis.com/css?family=Oxygen" rel="stylesheet">
+              <link href="https://fonts.googleapis.com/css?family=Oxygen|Quicksand" rel="stylesheet">
               <title>Theme 4</title>
               <style>
                   body{font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif;}
@@ -20,14 +20,17 @@
               .texte2{
                 width: 200px;
               }
-              .main{ padding:0;margin:0;width: 800px;height:420px;position: relative;overflow: hidden; background: #FFF;}
+              .main{ padding:0;margin:0;width: 800px;height:420px;position: relative;overflow: hidden; background: #3e4980; }
 .main img{ position:absolute; max-height:420px; max-width:800px; }
 
-#sde{position:absolute; z-index:1; right:0; bottom:0;  max-width:800px; max-height:420px; }
+#sde{position:absolute; z-index:1; right:0; bottom:0;  max-width:800px; height:420px; }
 
-#texte{z-index:1; font-family: 'Oxygen', sans-serif; position: absolute; left: 0px; top: 170px; color:#000; font-size:35px; line-height:40px; width:425px ; height:220px ; display:flex; justify-content:center; align-items:center; text-align:center;}
+#texte{z-index:1; font-family: 'Oxygen', sans-serif; position: absolute; left: 0px; top: 170px; color:#FFF; font-size:35px; line-height:45px; width:440px ; height:220px ; display:flex; justify-content:center; align-items:center; text-align:center;}
 
-#fb_id_user{position: absolute; z-index:1; left: 160px; top: 70px; width:130px ;  height:130px ; object-fit: cover; object-position: 50% 10%; border-radius:130px; }
+#fb_id_user{position: absolute; z-index:1; left: 160px; top: 40px; width:130px ;  height:130px ; object-fit: cover; object-position: 50% 10%; border-radius:130px; }
+
+#hashtag{z-index:1; font-family: 'Quicksand', sans-serif; font-weight:700; position: absolute; right: 10px; bottom: 5px; color:#000; font-size:20px; line-height:20px; width:200px ; height:30px ; text-align:right;   }
+
               </style>
               <script src='../../../src/js/jquery.js'></script>
               <script>
@@ -71,16 +74,21 @@
 <?php
           $textes = array(''.$_GET['user_name'].' a marre d\'attendre l\'eau jusqu\'à 03h du matin.', 
                     ''.$_GET['user_name'].' a marre de faire le tour des quartiers pour avoir de l\'eau.',
-                    ''.$_GET['user_name'].' réclame la disponiblité de l\'eau toute la journée.');
+                    ''.$_GET['user_name'].' réclame la disponiblité de l\'eau toute la journée.',
+                    ''.$_GET['user_name'].' dit NON aux longues périodes de pénurie d\'eau'
+                    );
           shuffle($textes);
 ?>
 <!DOCTYPE HTML>
 
-<img src="https://graph.facebook.com/<?php echo $_GET['fb_id_user']; ?>/picture/?width=275&height=275" class="img_profile" id="fb_id_user"> 
+<img src="<?php echo $_GET['url_img_profile_user']; ?>" class="img_profile" id="fb_id_user"> 
 
-<img src="https://creation.funizi.com/images-theme-perso/1528715570.jpg" id="sde"> 
+<img src="https://creation.funizi.com/images-theme-perso/1528719986.jpg" id="sde"> 
  
 <div class="texte" id="texte"> <?=$textes[0]?> </div>
+
+<div class="texte" id="hashtag">#dafadoy</div>
+ 
  
 
         </div>
