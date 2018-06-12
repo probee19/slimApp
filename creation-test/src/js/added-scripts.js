b53e24d67c446fa3f70b3e8a335bedd268e4d477
Pedre <pedre@3w.lagence.de.com>
2018-06-12 12:49:06 +0000

    Adding Citation Module

--- creation-test/src/js/added-scripts.js
+++ creation-test/src/js/added-scripts.js
@@ -223,4 +223,82 @@ $(document).ready(function(){
     });
 
 
+    $( "#ajout_citation_form" ).validate( {
+        ignore: "not:hidden",
+        rules: {
+            titre: "required",
+            rubrique: "required",
+        },
+        messages: {
+            titre: "Le titre de la citation est manquant",
+            rubrique: "Veulliez choisir une rubrique pour la citation"
+        },
+        errorElement: "em",
+        errorPlacement: function ( error, element ) {
+            // Ajout de la classe help-block pour les érreurs
+            error.addClass( "help-block" );
+            if ( element.prop( "type" ) === "checkbox" ) {
+                error.insertAfter( element.parent( "label" ) );
+            } else {
+                error.insertAfter( element );
+            }
+        },
+        highlight: function ( element, errorClass, validClass ) {
+            $( element ).parents( ".form-group" ).addClass( "has-error" ).removeClass( "has-success" );
+        },
+        unhighlight: function (element, errorClass, validClass) {
+            $( element ).parents( ".form-group" ).addClass( "has-success" ).removeClass( "has-error" );
+        },
+        submitHandler: function(e) {
+
+            //e.preventDefault(); // Le navigateur ne peut pas envoyer le formulaire
+            var ajaxData = new FormData();
+
+            // Ajout des autres données dans ajaxData
+            var other_data = $('form').serializeArray();
+            $.each(other_data,function(key,input){
+                ajaxData.append(input.name,input.value);
+            });
+            //saveCitation(data);
+
+            var codePHPHTML = editorHTML.getSession().getValue();
+          	var codeCSS = editorCSS.getSession().getValue();
+          	var codeJS = editorJS.getSession().getValue();
+          	var codeRequireTop = editorRequireTop.getSession().getValue();
+          	var codeRequireBottom = editorRequireBottom.getSession().getValue();
+
+          	ajaxData.append('codePHPHTML', codePHPHTML);
+          	ajaxData.append('codeCSS', codeCSS);
+          	ajaxData.append('codeJS', codeJS);
+          	ajaxData.append('codeRequireTop', codeRequireTop);
+          	ajaxData.append('codeRequireBottom', codeRequireBottom);
+            $.ajax({
+                url: "save",
+                type: "POST",
+                data : ajaxData,
+                processData: false,
+                contentType: false,
+                beforeSend: function(){
+                    // Bouton désactivé en attendant le traitement
+                    $('#btn_save_citation').prop( "disabled", true ).html("Traitement en cours...").css("color","#000");
+                    $('#btn_cancel').prop( "disabled", true );
+                    $("html, body").animate({ scrollTop: 0 }, 600);
+                    loadingProgress ('0%', 'Création de la citation en cours...');
+                }
+                }).done(function( data ) {
+                    // Bouton réactivé, formulaire réinitialisé
+                    console.log(data);
+                    setTimeout(function(){
+                      loadingProgress ('100%', 'Citation enrégistrée avec succès.');
+                      $(".alert-success").fadeIn("slow");
+                    },2000);
+
+                    $("html, body").animate({ scrollTop: 0 }, 600);
+                    setTimeout(function(){ window.location = "https://creation.funizi.com/citations"; }, 4000);
+                });
+            return false;
+        }
+    });
+    // Fin validation
+
 });
