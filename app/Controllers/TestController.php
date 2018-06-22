c93b25dcbc2b3366cd38d415aa7e920dcfbb8155
Pedre <pedre@3w.lagence.de.com>
2018-06-22 10:15:30 +0000

    Code optimization

--- app/Controllers/TestController.php
+++ app/Controllers/TestController.php
@@ -28,9 +28,10 @@ class TestController extends Controller
         if($args['code']){
           $code = $args['code'];
           $img_url = "/uploads/".$code.".jpg";
+          $user_test = UserTest::where('uuid', '=', "$code")->first();
+          $img_url = $user_test->img_url;
         }
-        //$user_test = UserTest::where('uuid', '=', "$code")->first();
-        //$img_url = $user_test->img_url;
+
 
         $test = Test::selectRaw('tests.statut AS statut, tests.id_rubrique AS id_rubrique, tests.titre_test AS titre_test_fr, tests.if_additionnal_info AS if_additionnal_info, tests.permissions AS permissions, tests.id_test AS id_test, tests.url_image_test AS url_image_test, test_info.lang AS lang, test_info.titre_test AS titre_test')
             ->join('test_info','test_info.id_test','tests.id_test')
