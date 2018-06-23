3cd1f992f9d66235449ab5719e18468cf14b78c1
Pedre <pedre@3w.lagence.de.com>
2018-06-23 12:23:28 +0000

    Update FCE url

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -591,6 +591,7 @@ class Helper
          $alllang [] = [
            "id"          =>  $lang->id,
            "code"        =>  $lang->code,
+           "name"        =>  $lang->name,
            "fr_name"     =>  $lang->fr_name,
            "status"      =>  $lang->status,
            "translated"  =>  $lang->translated,
