c92bf8809d2830f0e2bf8f2587d25284602a396b
Pedre <pedre@3w.lagence.de.com>
2018-08-08 16:37:32 +0000

    Updating GeoPlugin

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -28,7 +28,7 @@ use Aws\S3\S3Client;
 
 class Helper
 {
-    public static function getCountry($ip){
+    public static function getCountry_2($ip){
         if(isset($_COOKIE['countryCode'], $_COOKIE['countryName'])){
             $country = [
                 'countryName'   =>  $_COOKIE['countryName'],
@@ -75,12 +75,11 @@ class Helper
     }
 
     public function is_bot($user_agent) {
-
         $botRegexPattern = "(googlebot\/|Googlebot\-Mobile|Googlebot\-Image|Google favicon|Mediapartners\-Google|bingbot|slurp|java|wget|curl|Commons\-HttpClient|Python\-urllib|libwww|httpunit|nutch|phpcrawl|msnbot|jyxobot|FAST\-WebCrawler|FAST Enterprise Crawler|biglotron|teoma|convera|seekbot|gigablast|exabot|ngbot|ia_archiver|GingerCrawler|webmon |httrack|webcrawler|grub\.org|UsineNouvelleCrawler|antibot|netresearchserver|speedy|fluffy|bibnum\.bnf|findlink|msrbot|panscient|yacybot|AISearchBot|IOI|ips\-agent|tagoobot|MJ12bot|dotbot|woriobot|yanga|buzzbot|mlbot|yandexbot|purebot|Linguee Bot|Voyager|CyberPatrol|voilabot|baiduspider|citeseerxbot|spbot|twengabot|postrank|turnitinbot|scribdbot|page2rss|sitebot|linkdex|Adidxbot|blekkobot|ezooms|dotbot|Mail\.RU_Bot|discobot|heritrix|findthatfile|europarchive\.org|NerdByNature\.Bot|sistrix crawler|ahrefsbot|Aboundex|domaincrawler|wbsearchbot|summify|ccbot|edisterbot|seznambot|ec2linkfinder|gslfbot|aihitbot|intelium_bot|facebookexternalhit|yeti|RetrevoPageAnalyzer|lb\-spider|sogou|lssbot|careerbot|wotbox|wocbot|ichiro|DuckDuckBot|lssrocketcrawler|drupact|webcompanycrawler|acoonbot|openindexspider|gnam gnam spider|web\-archive\-net\.com\.bot|backlinkcrawler|coccoc|integromedb|content crawler spider|toplistbot|seokicks\-robot|it2media\-domain\-crawler|ip\-web\-crawler\.com|siteexplorer\.info|elisabot|proximic|changedetection|blexbot|arabot|WeSEE:Search|niki\-bot|CrystalSemanticsBot|rogerbot|360Spider|psbot|InterfaxScanBot|Lipperhey SEO Service|CC Metadata Scaper|g00g1e\.net|GrapeshotCrawler|urlappendbot|brainobot|fr\-crawler|binlar|SimpleCrawler|Livelapbot|Twitterbot|cXensebot|smtbot|bnf\.fr_bot|A6\-Indexer|ADmantX|Facebot|Twitterbot|OrangeBot|memorybot|AdvBot|MegaIndex|SemanticScholarBot|ltx71|nerdybot|xovibot|BUbiNG|Qwantify|archive\.org_bot|Applebot|TweetmemeBot|crawler4j|findxbot|SemrushBot|yoozBot|lipperhey|y!j\-asr|Domain Re\-Animator Bot|AddThis|YisouSpider|BLEXBot|YandexBot|SurdotlyBot|AwarioRssBot|FeedlyBot|Barkrowler|Gluten Free Crawler|Cliqzbot)";
         return preg_match("/{$botRegexPattern}/", $user_agent);
     }
-    
-    public static function getCountry_2($ip){
+
+    public static function getCountry($ip){
         if(isset($_COOKIE['countryCode'], $_COOKIE['countryName'])){
             $country = [
                 'countryName'   =>  $_COOKIE['countryName'],
@@ -121,7 +120,6 @@ class Helper
         return $country;
     }
 
-
     public static function debug($var){
         echo "<pre>";
         var_dump($var);
@@ -147,7 +145,7 @@ class Helper
         return preg_replace('/-+/', '-', $url);
     }
 
-    public function getCountryCode(){
+    public function getCountryCode_2(){
       if(isset($_COOKIE["country_user"]))
           $country = (array) json_decode($_COOKIE['country_user']);
       else {
@@ -158,8 +156,7 @@ class Helper
       return $country['countryCode'];
     }
 
-
-    public function getCountryCode_2(){
+    public function getCountryCode(){
       if(isset($_COOKIE["country_user"]))
           $country = (array) json_decode($_COOKIE['country_user']);
       else {
